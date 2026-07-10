e1c4ad950be0da35aa2ea9788841fac33c4eeddc
Phu Tuong Long <bnm915915@gmail.com>
2026-07-10 13:00:18 +0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+    const cleanString = s
+      .replaceAll(/[^a-zA-Z0-9]/g, "")
+      .toLowerCase()
+      .trim();
+    const reverseString = cleanString.split("").reverse().join("");
+    return reverseString === cleanString;
+    }
+}
